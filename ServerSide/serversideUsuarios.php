c72c230cdcbf108547b27599c5ec437b5a3e2049
Kevin Lopez <kevinlopeztorres@hotmail.com>
2021-05-02 12:51:34 -0500

    primer commit

---
+++ ServerSide/serversideUsuarios.php
@@ -0,0 +1,5 @@
+<?php
+require 'serverside.php';
+$table_data->get('mig_data','codbien',array('codbien', 'descripcio','serie','marca','modelo','codlocal','ubicacion','nombreubicacion','codusuario'));
+
+?>	
